8942dfeaf7cb90183559c1d75a6e85c52967611d
Sarah Holden <sarahbethholden@gmail.com>
2024-02-02 15:03:47 -0800

    Updating from state -> const

--- activity-stats/src/components/ActivityStats/ActivityStats.tsx
+++ activity-stats/src/components/ActivityStats/ActivityStats.tsx
@@ -22,6 +22,48 @@ type CombinedData = {
 // What constitutes a transaction?
 // fiat currency - can it differ? Does it need to first be converted to dollars and then totalled?
 
+export const combineDataRows = (
+  data: ActivityData[]
+): Map<string, CombinedData> => {
+  const combinedData = new Map();
+
+  for (const {
+    type,
+    count,
+    amount_fiat,
+    fiat_currency,
+    crypto_currency,
+  } of data) {
+    if (combinedData.has(type)) {
+      // Existing entry updates
+      const prevData = combinedData.get(type);
+
+      const newCombined = {
+        ...prevData,
+        count: prevData.count + count,
+        amount_fiat: prevData.amount_fiat + Number(amount_fiat),
+        crypto_currencies: prevData.crypto_currencies.has(crypto_currency)
+          ? prevData.crypto_currencies
+          : prevData.crypto_currencies.add(crypto_currency),
+      };
+      combinedData.set(type, newCombined);
+    } else {
+      // New Entry Creation
+      combinedData.set(type, {
+        count,
+        amount_fiat: Number(amount_fiat),
+        fiat_currency,
+        type,
+        crypto_currencies: new Set([crypto_currency]),
+      });
+    }
+  }
+
+  return combinedData;
+};
+
+const ACTIVITY_DATA = combineDataRows(mockActivityStatsData);
+
 export const ActivityRow = ({ rowData }: { rowData: CombinedData }) => {
   const { type, count, fiat_currency, amount_fiat, crypto_currencies } =
     rowData;
@@ -46,10 +88,6 @@ export const ActivityRow = ({ rowData }: { rowData: CombinedData }) => {
 };
 
 const ActivityStats = () => {
-  const [activityData] = useState<Map<string, CombinedData>>(() => {
-    return combineDataRows(mockActivityStatsData);
-  });
-
   return (
     <div className="activity">
       <h1>Activity</h1>
@@ -60,9 +98,9 @@ const ActivityStats = () => {
             View Report
           </button>
         </div>
-        {activityData ? (
+        {ACTIVITY_DATA ? (
           <div className="activity__rows" role="list">
-            {Array.from(activityData.entries()).map(([type, info]) => {
+            {Array.from(ACTIVITY_DATA.entries()).map(([type, info]) => {
               return <ActivityRow key={type} rowData={info} />;
             })}
           </div>
@@ -81,42 +119,6 @@ export const formatCurrency = (amount: number, currency: string) => {
   });
 
   return currencyFormat.format(Number(amount));
-};
-
-export const combineDataRows = (
-  data: ActivityData[]
-): Map<string, CombinedData> => {
-  const combinedData = new Map();
-
-  data.forEach(
-    ({ type, count, amount_fiat, fiat_currency, crypto_currency }) => {
-      if (combinedData.has(type)) {
-        // Existing entry updates
-        const prevData = combinedData.get(type);
-
-        const newCombined = {
-          ...prevData,
-          count: prevData.count + count,
-          amount_fiat: prevData.amount_fiat + Number(amount_fiat),
-          crypto_currencies: prevData.crypto_currencies.has(crypto_currency)
-            ? prevData.crypto_currencies
-            : prevData.crypto_currencies.add(crypto_currency),
-        };
-        combinedData.set(type, newCombined);
-      } else {
-        // New Entry Creation
-        combinedData.set(type, {
-          count,
-          amount_fiat: Number(amount_fiat),
-          fiat_currency,
-          type,
-          crypto_currencies: new Set([crypto_currency]),
-        });
-      }
-    }
-  );
-
-  return combinedData;
 };
 
 export default ActivityStats;
